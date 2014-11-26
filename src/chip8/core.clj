90ae6ce6552c8bf0d837f5803edf8df0b1e8332f
Roman Flammer <phalphalak@gmail.com>
2014-11-26 01:29:39 +0100

    Add opcode.

--- src/chip8/core.clj
+++ src/chip8/core.clj
@@ -10,28 +10,67 @@
       (.read bis buffer 0 length)
       buffer)))
 
+
+(defn reset [vm]
+  ; memory to zero
+  )
+
 (defn create-vm []
   {:display {:width 64
              :height 32
              :pixels (vec (boolean-array (* 64 32)))}
+   :error nil
+   :clock-speed 60 ;; hertz
+   :pc 0x200
    ;; memory
    ;;     0 - 0x200 : interpreter or font data in modern chip 8s
    ;; 0xea0 - 0xeff : call stack
    ;; 0xf00 - 0xfff : display refresh
    :memory (vec (byte-array 0x1000))
-   :registers (vec (byte-array 16))
-   :address-register (byte 0)
+   :registers (vec (byte-array 16)) ;; a.k.a. V0 to VF (last one acts as a carry over)
+   :address-register (byte 0) ;; a.k.a. I
    ;; timers run at 60 hertz
    :timers {:delay 0
             :sound 0}
    :keyboard (vec (boolean-array 16))})
 
-(defn- set-memory [vm index byte]
+#_(defn- set-memory [vm index byte]
   (assoc-in vm [:memory index] byte))
+
+(defn memory [vm index]
+  {:pre [(pos? index) (< index (count (:memory vm)))]}
+  (bit-and 0xff (get-in vm [:memory index])))
+
+(defn pc [vm]
+  (:pc vm))
+
+(defn opcode [vm]
+  (bit-or (bit-shift-left (memory vm (pc vm))
+                          8)
+          (memory vm (inc (pc vm)))))
+
+(defn increment-pc [vm]
+      (update-in vm :pc + 2))
+
+(defn process-opcode [vm opcode]
+  :todo
+  vm)
+
+
+(defn step [vm]
+  (-> vm
+      (process-opcode (opcode vm))
+      increment-pc))
 
 (defn load-program [vm f]
   (let [bytes (vec (file->bytes f))
-        len (count bytes)]
+        len (count bytes)
+        mem (:memory vm)]
     (assoc vm
-      :memory (vec (concat bytes
-                           (drop len (:memory vm)))))))
+      :memory (vec (concat (take 0x200 mem)
+                           bytes
+                           (drop (+ 0x200 len) mem))))))
+
+
+(comment
+  (load-program (create-vm) "resources/Maze [David Winter, 199x].ch8"))
